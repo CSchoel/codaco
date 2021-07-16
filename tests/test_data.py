fc02c7ca3247d168036d80a19a9eacacd1404105
Christopher Schölzel <christopher.schoelzel@gmx.net>
2021-07-16 16:45:00 +0200

    moves group extraction into second phase

--- tests/test_data.py
+++ tests/test_data.py
@@ -11,10 +11,12 @@ class TestUCImlr(unittest.TestCase):
             re.search(
                 r'"..\/machine-learning-databases\/(.*[^/])\/?"',
                 requests.get("https://archive.ics.uci.edu/ml/datasets/" + n).text
-            ).group(1)
+            )
             for n in names
         ]
+        # get id where match was successful
+        ids = [x.group(1) for x in ids if x is not None]
         for i in ids:
-            print(n)
+            print(i)
         data = cd.load_dataset("abalone", source="ucimlr")
         print(data)
