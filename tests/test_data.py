d9f11cfaf668a25a425d4e050e79d8281bf13da6
Christopher Schölzel <christopher.schoelzel@gmx.net>
2021-07-16 16:33:23 +0200

    gets dataset ids from names

--- tests/test_data.py
+++ tests/test_data.py
@@ -7,7 +7,14 @@ class TestUCImlr(unittest.TestCase):
     def test_guess_ucimlr_columns(self):
         datasets = requests.get("https://archive.ics.uci.edu/ml/datasets.php").text
         names = re.findall(r'<a href="datasets\/(.*)"><img ', datasets)
-        for n in names:
+        ids = [
+            re.search(
+                r'"..\/machine-learning-databases\/(.*[^/])\/?"',
+                requests.get("https://archive.ics.uci.edu/ml/datasets/" + n).text
+            ).group(1)
+            for n in names
+        ]
+        for i in ids:
             print(n)
         data = cd.load_dataset("abalone", source="ucimlr")
         print(data)
