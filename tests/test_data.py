f4d66c45617bd07aa94249d9453f3374e140821e
Christopher Schölzel <christopher.schoelzel@gmx.net>
2021-07-16 16:55:33 +0200

    caches ids on disc and loads all datasets if possible

--- tests/test_data.py
+++ tests/test_data.py
@@ -2,21 +2,26 @@ import unittest
 import codaco.data as cd
 import requests
 import re
+import pathlib
 
 class TestUCImlr(unittest.TestCase):
     def test_guess_ucimlr_columns(self):
         datasets = requests.get("https://archive.ics.uci.edu/ml/datasets.php").text
         names = re.findall(r'<a href="datasets\/(.*)"><img ', datasets)
-        ids = [
-            re.search(
-                r'"..\/machine-learning-databases\/(.*[^/])\/?"',
-                requests.get("https://archive.ics.uci.edu/ml/datasets/" + n).text
-            )
-            for n in names
-        ]
-        # get id where match was successful
-        ids = [x.group(1) for x in ids if x is not None]
+        idfile = pathlib.Path("datasets/ucimlr.ids")
+        if idfile.exists():
+            ids = idfile.read_text(encoding="utf-8")
+        else:
+            ids = [
+                re.search(
+                    r'"..\/machine-learning-databases\/(.*[^/])\/?"',
+                    requests.get("https://archive.ics.uci.edu/ml/datasets/" + n).text
+                )
+                for n in names
+            ]
+            # get id where match was successful
+            ids = [x.group(1) for x in ids if x is not None]
+            idfile.write_text("\n".join(ids), encoding="utf-8")
         for i in ids:
             print(i)
-        data = cd.load_dataset("abalone", source="ucimlr")
-        print(data)
+            data = cd.load_dataset(i, source="ucimlr")
