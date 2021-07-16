f34b32a9abb83c28419f5ade0650ad85157c1fc9
Christopher Schölzel <christopher.schoelzel@gmx.net>
2021-07-16 16:15:54 +0200

    lists als datasets in the UCIMLR

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,7 +1,13 @@
 import unittest
 import codaco.data as cd
+import requests
+import re
 
 class TestUCImlr(unittest.TestCase):
     def test_guess_ucimlr_columns(self):
+        datasets = requests.get("https://archive.ics.uci.edu/ml/datasets.php").text
+        names = re.findall(r'<a href="datasets\/(.*)"><img ', datasets)
+        for n in names:
+            print(n)
         data = cd.load_dataset("abalone", source="ucimlr")
         print(data)
