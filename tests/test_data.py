0c6febe438d0bd70805b8e87ed0c02d7c41691f8
Christopher Schölzel <christopher.schoelzel@gmx.net>
2022-02-02 20:43:40 +0100

    revises tests into actual test cases with hypothesis

--- tests/test_data.py
+++ tests/test_data.py
@@ -2,15 +2,50 @@ import unittest
 import codaco.data as cd
 import requests
 import re
-import pathlib
+from pathlib import Path
+import tempfile
+import shutil
 
 class TestUCImlr(unittest.TestCase):
-    def test_guess_ucimlr_columns(self):
-        outdir = pathlib.Path("~/temp/codaco").expanduser()
-        outdir.mkdir(exist_ok=True, parents=True)
-        # only requires download of a few files with no subfolders
-        data = cd.load_dataset("abalone", source="ucimlr", download_to=outdir, force_download=True)
-        # requires recursive download
-        data = cd.load_dataset("mechanical-analysis", source="ucimlr", download_to=outdir, force_download=True)
-        # requires recursive extraction
-        data = cd.load_dataset("UNIX_user_data-mld", source="ucimlr", download_to=outdir, force_download=True)
+    def setUp(self):
+        self.data_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.data_dir)
+
+    def test_download_recursive(self):
+        """
+        Test hypothesis: download_recursive fails to download content in subfolders
+        """
+        data = cd.load_dataset("mechanical-analysis", source="ucimlr", download_to=self.data_dir, force_download=True)
+        expected =  {
+            Path(self.data_dir) / 'mechanical-analysis/older-version/mechanical-analysis.notused-instances',
+            Path(self.data_dir) / 'mechanical-analysis/older-version/mechanical-analysis.names',
+            Path(self.data_dir) / 'mechanical-analysis/older-version/mechanical-analysis.data',
+            Path(self.data_dir) / 'mechanical-analysis/older-version/Index',
+            Path(self.data_dir) / 'mechanical-analysis/PUMPS-DATA-SET/DISTRIBUTION',
+            Path(self.data_dir) / 'mechanical-analysis/PUMPS-DATA-SET/Index',
+            Path(self.data_dir) / 'mechanical-analysis/Index'
+        }
+        self.assertEqual(expected, set(cd.walk(Path(self.data_dir) / "mechanical-analysis")))
+
+    def test_extract_recursive(self):
+        """
+        Test hypothesis: extract_recursive fails to extract archives that are contained within another archive
+        """
+        data = cd.load_dataset("UNIX_user_data-mld", source="ucimlr", download_to=self.data_dir, force_download=True)
+        expected = {
+            Path(self.data_dir) / 'UNIX_user_data-mld/README',
+            Path(self.data_dir) / 'UNIX_user_data-mld/UNIX_user_data.html',
+            Path(self.data_dir) / 'UNIX_user_data-mld/UNIX_user_data/README',
+            Path(self.data_dir) / 'UNIX_user_data-mld/UNIX_user_data/USER0',
+            Path(self.data_dir) / 'UNIX_user_data-mld/UNIX_user_data/USER1',
+            Path(self.data_dir) / 'UNIX_user_data-mld/UNIX_user_data/USER2',
+            Path(self.data_dir) / 'UNIX_user_data-mld/UNIX_user_data/USER3',
+            Path(self.data_dir) / 'UNIX_user_data-mld/UNIX_user_data/USER4',
+            Path(self.data_dir) / 'UNIX_user_data-mld/UNIX_user_data/USER5',
+            Path(self.data_dir) / 'UNIX_user_data-mld/UNIX_user_data/USER6',
+            Path(self.data_dir) / 'UNIX_user_data-mld/UNIX_user_data/USER7',
+            Path(self.data_dir) / 'UNIX_user_data-mld/UNIX_user_data/USER8'
+        }
+        self.assertEqual(expected, set(cd.walk(Path(self.data_dir) / "UNIX_user_data-mld")))
