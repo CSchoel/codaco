8ed7033cf50d327b7f0dd448944f9490787bc76a
Christopher Schölzel <christopher.schoelzel@gmx.net>
2022-02-02 20:07:11 +0100

    uses smaller mechanical_analysis instead of chess and adds recursive extraction test

--- tests/test_data.py
+++ tests/test_data.py
@@ -8,5 +8,9 @@ class TestUCImlr(unittest.TestCase):
     def test_guess_ucimlr_columns(self):
         outdir = pathlib.Path("~/temp/codaco").expanduser()
         outdir.mkdir(exist_ok=True, parents=True)
+        # only requires download of a few files with no subfolders
         data = cd.load_dataset("abalone", source="ucimlr", download_to=outdir, force_download=True)
-        data = cd.load_dataset("chess", source="ucimlr", download_to=outdir, force_download=True)
+        # requires recursive download
+        data = cd.load_dataset("mechanical-analysis", source="ucimlr", download_to=outdir, force_download=True)
+        # requires recursive extraction
+        data = cd.load_dataset("UNIX_user_data-mld", source="ucimlr", download_to=outdir, force_download=True)
