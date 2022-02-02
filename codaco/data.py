161ac7e406501641ebec9a88bf1687c2dd3ded47
Christopher Schölzel <christopher.schoelzel@gmx.net>
2022-02-02 22:43:16 +0100

    saves last line instead of current when streak is broken

--- codaco/data.py
+++ codaco/data.py
@@ -177,12 +177,11 @@ def find_table_block(text: str, tabsize: int=4):
         while j in colcount:
             del colcount[j]
             j += 1
-        colsum = sum(colcount.values())
         # check if we are at the end of a consecutive run
         # => i.e. the maximum runlenght of the previous line was higher
         if max(lastline.values(), default=0) > max(colcount.values(), default=0):
             # no continuing lines found
-            found.append((i, colsum))
+            found.append((i-1, sum(lastline.values())))
         lastline = colcount
     print(found)
     best = max(found, key=lambda x: x[1], default=(0, 0))
