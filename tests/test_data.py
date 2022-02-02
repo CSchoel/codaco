97dd2ba1fec3ad38a9957936b1a59a4bf626b51e
Christopher Schölzel <christopher.schoelzel@gmx.net>
2022-02-02 18:54:59 +0100

    changes test since downloading whole UCIMLR is not feasible anyway

--- tests/test_data.py
+++ tests/test_data.py
@@ -8,28 +8,5 @@ class TestUCImlr(unittest.TestCase):
     def test_guess_ucimlr_columns(self):
         outdir = pathlib.Path("~/temp/codaco").expanduser()
         outdir.mkdir(exist_ok=True, parents=True)
-        datasets = requests.get("https://archive.ics.uci.edu/ml/datasets.php").text
-        names = re.findall(r'<a href="datasets\/(.*)"><img ', datasets)
-        idfile = outdir.joinpath("ucimlr.ids")
-        if idfile.exists():
-            ids = idfile.read_text(encoding="utf-8").splitlines()
-        else:
-            ids = [
-                re.search(
-                    r'"..\/machine-learning-databases\/(.*[^/])\/?"',
-                    requests.get("https://archive.ics.uci.edu/ml/datasets/" + n).text
-                )
-                for n in names
-            ]
-            # get id where match was successful
-            ids = [x.group(1) for x in ids if x is not None]
-            idfile.write_text("\n".join(ids), encoding="utf-8")
-        exceptional = []
-        for i in ids:
-            print(i)
-            try:
-                data = cd.load_dataset(i, source="ucimlr", download_to=outdir)
-            except Exception as e:
-                print(f"ERROR on dataset {i}: {e}")
-                exceptional.append(i)
-        print(f"Exceptional IDs: {exceptional}")
+        data = cd.load_dataset("abalone", source="ucimlr", download_to=outdir)
+        data = cd.load_dataset("chess", source="ucimlr", download_to=outdir)
