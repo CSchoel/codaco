fdcca233954d1f46826786ff2ddfb138a7075501
Christopher Schölzel <christopher.schoelzel@gmx.net>
2022-02-03 18:36:26 +0100

    fixes data type of default value

--- codaco/data.py
+++ codaco/data.py
@@ -212,7 +212,7 @@ def find_table_block(text: str, tabsize: int=4):
                 # colum height is at least 3
                 found.append((i-1, max_cells(select_columns(lastline))))
         lastline = colcount
-    best = max(found, key=lambda x: x[1], default=(0, [], 0))
+    best = max(found, key=lambda x: x[1], default=(0, (0, [], 0)))
     block = "\n".join(x for i, x in enumerate(text.splitlines()) if i <= best[0] and i > best[0] - best[1][2])
     print(block)
     if best[1][0] < 10:
