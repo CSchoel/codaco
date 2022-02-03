b1bb6698dabe4108c36a8a772dbfa7c9c0e7e881
Christopher Schölzel <christopher.schoelzel@gmx.net>
2022-02-03 17:30:23 +0100

    only counts columns with maximum height

--- codaco/data.py
+++ codaco/data.py
@@ -181,7 +181,7 @@ def find_table_block(text: str, tabsize: int=4):
         # => i.e. the maximum runlenght of the previous line was higher
         if max(lastline.values(), default=0) > max(colcount.values(), default=0):
             # no continuing lines found
-            found.append((i-1, sum(lastline.values())))
+            found.append((i-1, sum(x for x in lastline.values() if x == max(lastline.values()))))
         lastline = colcount
     print(found)
     best = max(found, key=lambda x: x[1], default=(0, 0))
