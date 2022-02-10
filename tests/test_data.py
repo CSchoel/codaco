84ad9057adf1a29946124e4baab1efb6aef338cf
Christopher Schölzel <christopher.schoelzel@gmx.net>
2022-02-10 18:41:29 +0100

    finishes tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import tempfile
 import shutil
 import codaco.stat as cs
+import pandas as pd
 
 class TestUCImlr(unittest.TestCase):
     def setUp(self):
@@ -62,13 +63,21 @@ class TestUCImlr(unittest.TestCase):
         """
         cd.download_ucimlr("abalone", self.data_dir)
         dat = cd.load_csv_data(self.data_dir / "abalone")
+        if not isinstance(dat, pd.DataFrame):
+            self.fail(f"Expected DataFrame, but got {type(dat)}")
+        cols = ['Sex', 'Length', 'Diameter', 'Height', 'Whole weight', 'Shucked weight', 'Viscera weight', 'Shell weight', 'Rings']
+        self.assertEqual(cols, list(dat.columns))
+        first = ['M', 0.455, 0.365, 0.095, 0.514, 0.2245, 0.101, 0.15, 15]
+        last = ['M', 0.71, 0.555, 0.195, 1.9485, 0.9455, 0.3765, 0.495, 12]
+        self.assertEqual(first, list(dat.iloc[0,:].values))
+        self.assertEqual(last, list(dat.iloc[-1,:].values))
         cs.inspect_attributes(dat)
-        print(dat)
 
-    def test_read_namefile(self):
+    def test_guess_column_names(self):
         """
         Test hypothesis: read_namefile fails to find attribute names in block formatted name file.
         """
         cd.download_ucimlr("abalone", self.data_dir)
         nf = cd.guess_column_names(Path(self.data_dir) / "abalone/abalone.names", nattrib=9)
-        print(nf)
+        cols = ['Sex', 'Length', 'Diameter', 'Height', 'Whole weight', 'Shucked weight', 'Viscera weight', 'Shell weight', 'Rings']
+        self.assertEqual(cols, list(nf))
