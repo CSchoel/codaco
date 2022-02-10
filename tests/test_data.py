30b5304f96fdb5f941c34081ab36d3c05c725433
Christopher Schölzel <christopher.schoelzel@gmx.net>
2022-02-10 22:36:57 +0100

    prints files and reported types

--- tests/test_data.py
+++ tests/test_data.py
@@ -63,10 +63,9 @@ class TestUCImlr(unittest.TestCase):
         """
         cd.download_ucimlr("abalone", self.data_dir)
         dat = cd.load_csv_data(self.data_dir / "abalone")
-        for k in dat:
-            print(k)
-            print(dat[k])
-        print(list(cd.walk(self.data_dir)))
+        import magic
+        for f in cd.walk(self.data_dir):
+            print(f, magic.from_file(f, mime=True))
         if not isinstance(dat, pd.DataFrame):
             self.fail(f"Expected DataFrame, but got {type(dat)}")
         cols = ['Sex', 'Length', 'Diameter', 'Height', 'Whole weight', 'Shucked weight', 'Viscera weight', 'Shell weight', 'Rings']
