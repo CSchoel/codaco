d745c5cabfb8f8b2ce5fbb44480a0de683f01a19
Christopher Schölzel <christopher.schoelzel@gmx.net>
2022-02-13 16:57:09 +0100

    experiments with different normality measures

--- codaco/stat.py
+++ codaco/stat.py
@@ -3,7 +3,16 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import shapiro, probplot
 
 def inspect_attributes(df: pd.DataFrame):
     df.hist(bins=30)
+    print(df)
+    numeric = df.filter(items=[c for c,d in zip(df.columns, df.dtypes) if d.kind in ['i', 'f']])
+    normality = numeric.apply(lambda x: pd.Series(shapiro(x), index=["statistic", "p-value"]))
+    print(normality)
     plt.show()
+    plt.close()
+    probplot(numeric)
+    plt.show()
+    plt.close()
