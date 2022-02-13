d3547c8484342b2fda4a13487f89741fbd7c9ae9
Christopher Schölzel <christopher.schoelzel@gmx.net>
2022-02-13 21:34:52 +0100

    finds better way to index outliers

--- codaco/stat.py
+++ codaco/stat.py
@@ -11,7 +11,8 @@ def inspect_attributes(df: pd.DataFrame, plot=True) -> pd.DataFrame:
     # 1. Choose only numeric columns
     numeric = df.filter(items=[c for c,d in zip(df.columns, df.dtypes) if d.kind in ['i', 'f']])
     # 2. Search for outliers
-    outliers = numeric[numeric.apply(zscore) > 3]
+    index = numeric.apply(lambda x: np.abs(zscore(x))) > 3
+    outliers = numeric[[np.any(r) for i,r in index.iterrows()]]
     print(outliers)
     # 1. check numeric columns for normality with shapiro-wilk or jarque-bera ...
     # NOTE shapiro-wilk will also reject very small deviations for large N (> 5000)
@@ -27,12 +28,12 @@ def inspect_attributes(df: pd.DataFrame, plot=True) -> pd.DataFrame:
     normality.join(pd.DataFrame({ 'qqr' : [qqr for (_, _), (_, _, qqr) in qqdata] }))
     # 2. Assess normality based on SW and QQ parameters
     print(normality.T)
-    plt.show()
-    plt.close()
-    plt.figure()
-    w = int(np.ceil(np.sqrt(len(numeric.columns))))
-    for i, (c, d) in enumerate(numeric.items()):
-        ax = plt.subplot(w, w, i + 1)
-        (osm, osr), (slope, intercept, r) = probplot(d, fit=True)
-    plt.show()
-    plt.close()
+    # plt.show()
+    # plt.close()
+    # plt.figure()
+    # w = int(np.ceil(np.sqrt(len(numeric.columns))))
+    # for i, (c, d) in enumerate(numeric.items()):
+    #     ax = plt.subplot(w, w, i + 1)
+    #     (osm, osr), (slope, intercept, r) = probplot(d, fit=True)
+    # plt.show()
+    # plt.close()
